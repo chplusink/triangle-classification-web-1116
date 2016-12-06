e72e3b9b4acb85e39b6a3e4368949881a23cb4c3
VIC LAM <viclam@VICs-MacBook-Pro.local>
2016-12-06 10:47:17 -0500

    meow meow

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_reader :sides, :equilateral, :isoceles, :scalene
+
+  def initialize(a,b,c)
+    @sides = [a,b,c].sort
+    # binding.pry
+    if sides.length < 3
+      raise TriangleError
+    elsif sides.any? {|side| side <= 0}
+      raise TriangleError
+    elsif (sides[2] >= (sides[0] + sides[1]))
+      raise TriangleError
+    end
+  end
+
+  def kind
+    if sides[0] == sides[2]
+      :equilateral
+    elsif sides[0] == sides[1] || sides[1] == sides[2]
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+end
+
+class TriangleError < StandardError
+  def message
+    'meow meow'
+  end
+
+  # def no_size
+  #   'Side lengths must be greater than 0, meow meow'
+  # end
 end
